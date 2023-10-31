b6b8f7e87ed5e3cb9aa7454659221a73da7ece46
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2023-10-31 14:51:25 +0100

    styles: adds style and structure to work experience

--- app/(site)/components/workExperience/WorkExperience.tsx
+++ app/(site)/components/workExperience/WorkExperience.tsx
@@ -2,36 +2,31 @@ import { getExperience } from '@/sanity/query';
 import type { ExperienceType } from '@/types';
 import Image from 'next/image';
 
+import styles from './WorkExperience.module.css';
+
 export default async function Job() {
 	const job: ExperienceType[] = await getExperience();
 
 	return (
-		<section className='mt-32'>
-			<div className='mb-16'>
+		<section className={styles.container}>
+			<div className={styles.title_section}>
 				<h2 className='font-semibold text-4xl mb-4'>Work Experience</h2>
 			</div>
 
-			<div className='flex flex-col gap-y-12'>
+			<div className={styles.work_experience}>
 				{job.map((data) => (
-					<div
-						key={data._id}
-						className='flex items-start lg:gap-x-6 gap-x-4 max-w-2xl relative before:absolute before:bottom-0 before:top-[4.5rem] before:left-7 before:w-[1px] before:h-[calc(100%-50px)] before:bg-zinc-800'
-					>
-						<a
-							href={data.url}
-							rel='noreferrer noopener'
-							className='min-h-[60px] min-w-[60px] rounded-md overflow-clip relative'
-						>
-							<Image src={data.logo} className='object-cover' alt={`${data.name} logo`} fill />
-						</a>
-						<div className='flex flex-col items-start'>
-							<h3 className='text-xl font-bold'>{data.name}</h3>
-							<p>{data.jobTitle}</p>
-							<small className='text-sm text-zinc-500 mt-2 tracking-widest uppercase'>
-								{data.startDate.toString()} - {data.endDate.toString()}
-							</small>
-							<p className='text-base text-zinc-400 my-4'>{data.description}</p>
+					<div key={data._id} className={styles.work_experience_card}>
+						<div className={styles.header}>
+							<Image src={data.logo} className={styles.logo} width={64} height={64} alt={`${data.name} logo`} />
+							<div>
+								<h3 className={styles.work_name}>{data.name}</h3>
+								<p className={styles.work_title}>{data.jobTitle}</p>
+								<small className={styles.work_date}>
+									{data.startDate.toString()} - {data.endDate.toString()}
+								</small>
+							</div>
 						</div>
+						<p className={styles.description}>{data.description}</p>
 					</div>
 				))}
 			</div>
