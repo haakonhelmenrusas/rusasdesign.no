915c54455a7cad7034a72cec7729178962ae505d
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2023-10-31 19:41:34 +0100

    style: styling experience section

--- app/(site)/components/workExperience/WorkExperience.tsx
+++ app/(site)/components/workExperience/WorkExperience.tsx
@@ -12,18 +12,23 @@ export default async function Job() {
 			<div className={styles.title_section}>
 				<h2 className='font-semibold text-4xl mb-4'>Work Experience</h2>
 			</div>
-
 			<div className={styles.work_experience}>
 				{job.map((data) => (
 					<div key={data._id} className={styles.work_experience_card}>
 						<div className={styles.header}>
-							<Image src={data.logo} className={styles.logo} width={64} height={64} alt={`${data.name} logo`} />
+							{data.url ? (
+								<Image src={data.logo} className={styles.logo} width={64} height={64} alt={`${data.name} logo`} />
+							) : (
+								<div className={styles.logo}></div>
+							)}
 							<div>
 								<h3 className={styles.work_name}>{data.name}</h3>
 								<p className={styles.work_title}>{data.jobTitle}</p>
-								<small className={styles.work_date}>
-									{data.startDate.toString()} - {data.endDate.toString()}
-								</small>
+								{data.url && (
+									<small className={styles.work_date}>
+										{data.startDate.toString()} - {data.endDate.toString()}
+									</small>
+								)}
 							</div>
 						</div>
 						<p className={styles.description}>{data.description}</p>
