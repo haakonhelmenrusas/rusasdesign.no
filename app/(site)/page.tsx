5560cfed3ebda66e731b00dd1efeb86fcc101b07
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2023-10-31 13:28:34 +0100

    adds link to about page

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,14 +1,10 @@
+import Link from 'next/link';
 import styles from './page.module.css';
 
 export default function Home() {
 	return (
 		<main className={styles.main}>
-			<div className={styles.description}>
-				<p>
-					Get started by editing&nbsp;
-					<code className={styles.code}>app/page.tsx</code>
-				</p>
-			</div>
+			<Link href='/about'>About</Link>
 		</main>
 	);
 }
