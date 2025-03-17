cf4bd42f748ad09b97e13de9766835c3ce775f49
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2025-03-17 19:37:45 +0100

    feat: add blog title to the page and update README with Netlify status badge

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/2a70f8ca-927e-4044-9a94-6abfc460a345/deploy-status)](https://app.netlify.com/sites/rusasdesign/deploys)
+
 # Rusås Design homepage and blog
 
 This is the source code for the Rusås Design homepage and blog. It is built with Next.js and Sanity.io.
--- app/(pages)/blogg/page.tsx
+++ app/(pages)/blogg/page.tsx
@@ -10,6 +10,7 @@ export default async function Blog() {
     <>
       <Nav />
       <BackButton />
+      <h1 className="text-4xl font-bold mb-8">Blogg</h1>
       <Suspense fallback={<Loading />}>
         <div className="mb-16 grid grid-cols-1 gap-y-8 md:grid-cols-2 md:gap-x-16 md:gap-y-16 lg:gap-x-16">
           {data?.map((post) => {
