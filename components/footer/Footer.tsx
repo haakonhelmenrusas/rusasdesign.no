92bc8ed5c08a20b62bd5df157cff8ece64876c7c
Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>
2025-06-07 15:17:17 +0200

    fix: adding social links
    Signed-off-by: Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,9 +1,19 @@
 export default function Footer() {
   return (
     <footer className="grow max-w-[420] lg:max-w-(--breakpoint-lg) mx-auto bg-gray-200 dark:text-gray-50 dark:bg-gray-800 border-accent-2 p-8">
-        <p className="text-lg font-medium">
+        <p className="text-m font-medium">
           &copy; 2025 Rusås Design
         </p>
+      <div className="flex items-center justify-center gap-4 w-full">
+        <a aria-label="LinkedIn link" href="https://www.linkedin.com/in/haakon-helmen-rusas/" target="_blank"
+           rel="noreferrer noopener">
+          LinkedIn
+        </a>
+        <a aria-label="Github link" href="https://github.com/haakonhelmenrusas" target="_blank"
+           rel="noreferrer noopener">
+          Github
+        </a>
+      </div>
     </footer>
   );
 }
