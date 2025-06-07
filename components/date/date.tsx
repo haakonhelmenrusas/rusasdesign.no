d3af839d00775ce419c97719f1babca495b7fdb3
Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>
2025-06-07 10:03:18 +0200

    fix: change lang for date component to norwegian
    Signed-off-by: Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>

--- components/date/date.tsx
+++ components/date/date.tsx
@@ -1,9 +1,10 @@
 import { format } from 'date-fns';
+import { nb } from 'date-fns/locale/nb'
 
 export default function DateComponent({ dateString }: { dateString: string }) {
   return (
     <time dateTime={dateString}>
-      {format(new Date(dateString), 'd LLLL, yyyy')}
+      {format(new Date(dateString), 'd LLLL, yyyy', { locale: nb })}
     </time>
   );
 }
