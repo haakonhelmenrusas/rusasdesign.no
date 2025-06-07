55a3ccbc5336aad36d472de86bbb628dff05a050
Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>
2025-06-07 09:37:52 +0200

    style: clean up footer
    Signed-off-by: Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,11 +1,9 @@
 export default function Footer() {
   return (
-    <footer className="grow max-w-[420] lg:max-w-(--breakpoint-lg) mx-auto bg-gray-200 dark:text-gray-50 dark:bg-gray-800 border-accent-2 mt-auto p-6">
-      <div className="container mx-auto px-5">
+    <footer className="grow max-w-[420] lg:max-w-(--breakpoint-lg) mx-auto bg-gray-200 dark:text-gray-50 dark:bg-gray-800 border-accent-2 p-8">
         <p className="text-lg font-medium">
           &copy; 2025 Rusås Design
         </p>
-      </div>
     </footer>
   );
 }
