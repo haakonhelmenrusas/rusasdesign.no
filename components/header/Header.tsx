b4c1c140629c42774dff82405d4f3afb38a8d847
Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>
2025-06-07 09:40:54 +0200

    fix: clean up props header
    Signed-off-by: Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,6 +1,6 @@
 
 interface HeaderProps {
-  title: string | null | undefined;
+  title: string;
   description: string;
 }
 
@@ -9,7 +9,7 @@ export default async function Header({ title, description }: HeaderProps) {
   return (
     <header className="my-24 flex flex-col lg:mb-12">
       <h1 className="text-balance mt-6 pb-3 text-4xl font-medium leading-tight tracking-tighter lg:pr-8 lg:text-6xl">
-        {title || ''}
+        {title}
       </h1>
       <h2 className="text-lg">
         <p
