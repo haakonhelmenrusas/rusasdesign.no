6c10c703b98b7fba5afc045ef94167a84709e243
Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>
2025-08-22 12:56:06 +0200

    fix: update max-width breakpoint for responsive layout
    - Adjusted `max-w` class in `<main>` to use `--breakpoint-xl` for improved layout scaling on larger screens.
    
    Signed-off-by: Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>

--- app/(pages)/layout.tsx
+++ app/(pages)/layout.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react';
 export default async function Layout({ children }: { children: ReactNode; }) {
   return (
     <>
-      <main className="grow max-w-[420] lg:max-w-(--breakpoint-lg) mx-auto px-5 mt-8">
+      <main className="grow max-w-[420] lg:max-w-(--breakpoint-xl) mx-auto px-5 mt-8">
         {children}
       </main>
       <Footer />
