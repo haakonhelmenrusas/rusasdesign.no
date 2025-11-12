6895f1880f3a02d509ba4738150c7a274f8277cf
Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>
2025-11-12 19:57:48 +0100

    refactor: enhance layout metadata and improve accessibility features
    Signed-off-by: Haakon Helmen Rusås <haakon.helmen.rusas@pm.me>

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,15 @@ export async function generateMetadata(): Promise<Metadata> {
   return {
     title: 'Rusås Design',
     description: 'En blogg fra en web utvikler',
+    metadataBase: new URL('https://rusasdesign.no'),
+    openGraph: {
+      title: 'Rusås Design',
+      description: 'En blogg fra en web utvikler',
+      url: 'https://rusasdesign.no',
+      siteName: 'Rusås Design',
+      locale: 'no_NO',
+      type: 'website',
+    }
   };
 }
 
@@ -19,18 +28,26 @@ const oswald = Oswald({
 });
 
 const notoSans = Noto_Sans({
-  variable: '--font-noto_sans',
+  variable: '--font-noto-sans',
   subsets: ['latin'],
   display: 'swap',
 });
 
 export default async function RootLayout({ children }: { children: ReactNode; }) {
   return (
-    <html lang="no-nb" className={`${oswald.variable} ${notoSans.variable}`}>
-    <body className="min-h-screen flex flex-col">
-    <ClarityInit />
-    {children}
-    </body>
+    <html lang="nb-NO" className={`${oswald.variable} ${notoSans.variable}`}>
+      <body className="antialiased min-h-screen flex flex-col">
+        <a
+          href="#content"
+          className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 z-50 px-3 py-2 bg-white dark:bg-black text-sm rounded"
+        >
+          Skip to content
+        </a>
+        <ClarityInit />
+        <main id="content" className="flex-1">
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
